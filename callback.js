aded8bca2f24c7ac0ef2894c52c99d306fa03ac4
KimMinJae <smallkdb@gmail.com>
2022-05-16 22:22:28 +0900

    add Promise, Async/await

--- callback.js
+++ callback.js
@@ -1,3 +1,4 @@
+//콜백함수 동기성 비동기성 알아보기
 function myFav() {
 	console.log(`My favorite is coding!`);
 }
@@ -21,4 +22,73 @@ function twiceButton() {
 	callbackButton();
 	AsyncButton();
 	console.log("twiceButton finish!");
+}
+
+//Promise 사용해보기
+const checkOdd = function (num) {
+	return new Promise((reslove, reject) => {
+		setTimeout(function () {
+			if (num % 2 == 0) {
+				reslove("짝수! 성공입니다!");
+			} else {
+				reject(Error("홀수! 실패입니다!"));
+			}
+		}, 1000);
+	});
+};
+
+function runCheckOddReslove() {
+	checkOdd(4).then(
+		function (text) {
+			console.log(text);
+		},
+		function (error) {
+			console.log(error);
+		}
+	);
+}
+
+function runCheckOddReject() {
+	checkOdd(5).then(
+		function (text) {
+			console.log(text);
+		},
+		function (error) {
+			console.log(error);
+		}
+	);
+}
+
+//Async await 사용해보기
+
+async function asyncCheckOdd(num) {
+	setTimeout(function () {
+		if (num % 2 == 0) {
+			reslove("짝수! 성공입니다!");
+		} else {
+			reject(Error("홀수! 실패입니다!"));
+		}
+	}, 1000);
+}
+
+function asyncRunCheckOddReslove() {
+	checkOdd(4).then(
+		function (text) {
+			console.log(text);
+		},
+		function (error) {
+			console.log(error);
+		}
+	);
+}
+
+function asyncRunCheckOddReject() {
+	checkOdd(5).then(
+		function (text) {
+			console.log(text);
+		},
+		function (error) {
+			console.log(error);
+		}
+	);
 }
